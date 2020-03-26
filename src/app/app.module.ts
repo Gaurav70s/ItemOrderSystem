9af43b32914c104e4a395a658fa68e9324c639ec
gauravsingh <gaurav.singh@nextgentel.com>
2020-03-26 09:24:07 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ItemsComponent } from './items/items.component';
+import {AppComponent} from './app.component';
+import {ItemsComponent} from './items/items.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { ItemDetailsComponent } from './item-details/item-details.component';
-import { CheckoutComponent } from './checkout/checkout.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ItemDetailsComponent} from './item-details/item-details.component';
+import {CheckoutComponent} from './checkout/checkout.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { BooktableComponent } from './booktable/booktable.component';
-import { CartComponent } from './cart/cart.component';
-import { LoginComponent } from './login/login.component';
-import { AppMenuComponent } from './app-menu/app-menu.component';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {BooktableComponent} from './booktable/booktable.component';
+import {CartComponent} from './cart/cart.component';
+import {LoginComponent} from './login/login.component';
+import {AppMenuComponent} from './app-menu/app-menu.component';
 // import { SocialLoginModule} from 'angularx-social-login';
 // import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider  } from 'angularx-social-login';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { SignupComponent } from './signup/signup.component';
-import { KitchenOrderDisplayComponent } from './kitchen-order-display/kitchen-order-display.component';
-import { ErrorInterceptor, JwtInterceptor } from './_helpers';
-import { OrdersDashboardComponent } from './orders-dashboard/orders-dashboard.component';
-import { CreateItemComponent } from './create-item/create-item.component';
-import { CreateItemCategoryComponent } from './create-item-category/create-item-category.component';
-import { CreateItemImagesComponent } from './create-item-images/create-item-images.component';
-import { IngredientsComponent } from './ingredients/ingredients.component';
-import { CounterViewComponent } from './counter-view/counter-view.component';
-import { InvoiceComponent } from './invoice/invoice.component';
-import { PDFExportModule } from '@progress/kendo-angular-pdf-export';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SubscriptionModalComponent } from './subscription-modal/subscription-modal.component';
-import { OrderSnackbarComponent} from './order.snackbar/order.snackbar.component';
+import {ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import {SignupComponent} from './signup/signup.component';
+import {KitchenOrderDisplayComponent} from './kitchen-order-display/kitchen-order-display.component';
+import {ErrorInterceptor, JwtInterceptor} from './_helpers';
+import {OrdersDashboardComponent} from './orders-dashboard/orders-dashboard.component';
+import {CreateItemComponent} from './create-item/create-item.component';
+import {CreateItemCategoryComponent} from './create-item-category/create-item-category.component';
+import {CreateItemImagesComponent} from './create-item-images/create-item-images.component';
+import {IngredientsComponent} from './ingredients/ingredients.component';
+import {CounterViewComponent} from './counter-view/counter-view.component';
+import {InvoiceComponent} from './invoice/invoice.component';
+import {PDFExportModule} from '@progress/kendo-angular-pdf-export';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SubscriptionModalComponent} from './subscription-modal/subscription-modal.component';
+import {OrderSnackbarComponent} from './order.snackbar/order.snackbar.component';
 import {NgbAccordionModule, NgbProgressbarModule} from "@ng-bootstrap/ng-bootstrap";
-import { NgHttpLoaderModule } from 'ng-http-loader';
-import {MatDialog, MatDialogModule} from "@angular/material/dialog";
+import {NgHttpLoaderModule} from 'ng-http-loader';
+import {MatDialogModule} from "@angular/material/dialog";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
 import {CookieService} from "ngx-cookie-service";
-import { TableSelectComponent } from './table-select/table-select.component';
+import {TableSelectComponent} from './table-select/table-select.component';
 import {MatMenuModule} from "@angular/material/menu";
-import { PaymentComponent } from './payment/payment.component';
+import {PaymentComponent} from './payment/payment.component';
 import {MatListModule} from "@angular/material/list";
-import { CompletedOrderOnTableComponent } from './completed-order-on-table/completed-order-on-table.component';
-
+import {CompletedOrderOnTableComponent} from './completed-order-on-table/completed-order-on-table.component';
 
 
 /*const config = new AuthServiceConfig([
@@ -104,7 +103,6 @@ export function provideConfig() {
         MatCardModule,
         MatMenuModule,
         MatListModule,
-        MatDialog,
         NgbProgressbarModule
     ],
   providers: [
